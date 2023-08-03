8e7a9b75b311d5e8717ef8219bc76073a18b63c2
AlcariaJoshua <alcariajoshua13@gmail.com>
2023-08-03 15:57:31 +0800

    starsbuck

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'green':'#006341',
+        'black':'#000000',
+        'white':'#ffffff'
+      }
+    },
+  },
+  plugins: [],
+}
+
